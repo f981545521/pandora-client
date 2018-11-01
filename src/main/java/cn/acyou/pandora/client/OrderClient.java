242bcf5339ded36af05376866a7a3f6d6dcab1dd
youfang <981545521@qq.com>
2018-11-01 16:56:35 +0800

    add ：初始导入

---
+++ src/main/java/cn/acyou/pandora/client/OrderClient.java
@@ -0,0 +1,17 @@
+package cn.acyou.pandora.client;
+
+import org.springframework.cloud.netflix.feign.FeignClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ * @author youfang
+ * @version [1.0.0, 2018-11-01 下午 04:01]
+ **/
+@FeignClient(name = "service-order")
+public interface OrderClient {
+
+    @RequestMapping(value = "/info/getOrder", method = RequestMethod.POST)
+    public String getOrder();
+
+}
---
+++ src/main/java/cn/acyou/pandora/service/OrderCommonService.java
@@ -0,0 +1,25 @@
+package cn.acyou.pandora.service;
+
+import cn.acyou.pandora.client.OrderClient;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+/**
+ * @author youfang
+ * @version [1.0.0, 2018-11-01 下午 04:03]
+ **/
+@Component
+public class OrderCommonService {
+    private final static Logger logger = LoggerFactory.getLogger(OrderCommonService.class);
+
+    @Autowired
+    private OrderClient orderClient;
+
+    public String getInfoFromOrder(){
+        logger.info("OrderClient ------------->");
+        return orderClient.getOrder();
+    }
+
+}
